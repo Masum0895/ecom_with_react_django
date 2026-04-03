8eab980bc0c0273a34771448e02496feb4c1b53e
@Masum0895 <2221793@iub.edu.bd>
2026-04-03 18:55:38 +0600

    ProductDetail page added

--- frontend/components/ProductCard.jsx
+++ frontend/components/ProductCard.jsx
@@ -1,17 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { Link } from "react-router-dom";
 
-const ProductCard = ({product}) => {
-  
+function ProductCard({ product }) {
   const BASEURL = import.meta.env.VITE_DJANGO_BASE_URL;
-
-
   return (
-    <div className='bg-white rounded-xl shadow-md hover:shadow-lg hover:scale-[1.02] transition-transform p-4'>
-        <img src={`${BASEURL}${product.image}`} alt={product.name} className='w-full h-56 object-cover rounded-lg mb-4'/>
-        <h2 className='text-lg font-semibold text-gray-800 truncate'>{product.name || 'Unnamed Product'}</h2>
-        <p className="text-gray-600 font-medium">{ `${product.price} TK`}</p>
-    </div>
-  )
+    <Link to={`/product/${product.id}`}>
+      <div className="bg-white rounded-xl shadow-md hover:shadow-lg hover:scale-[1.02] transition-transform p-4 cursor-pointer">
+        <img
+          src={`${BASEURL}${product.image}`}
+          alt={product.name}
+          className="w-full h-56 object-cover rounded-lg mb-4"
+        />
+        <h2 className="text-lg font-semibold text-gray-800 truncate">
+          {product.name}
+        </h2>
+        <p className="text-gray-600 font-medium">${product.price}</p>
+      </div>
+    </Link>
+  );
 }
 
-export default ProductCard
+export default ProductCard;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import ProductList from "../pages/ProductList";
-
+import ProductDetails from "../pages/ProductDetails";
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 function App(){
    
   return(
-    <div>
-      <ProductList/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<ProductList/>}/>
+        <Route path="/product/:id" element={<ProductDetails/>}/>
+      </Routes>
+    </Router>
   );
 }
 
